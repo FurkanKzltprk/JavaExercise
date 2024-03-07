389d41b64da5bb37d3c5ef1ff374feeff2414a71
Furkan  kızıltoprak <kiziltoprakfurkan001@gmail.com>
2024-03-07 09:24:26 +0300

    hi1!

---
+++ Documents/NetBeansProjects/AlgoritmaKodCalismalari/src/algoritmakodcalismalari/AlgoritmaKodCalismalari.java
@@ -0,0 +1,10 @@
+
+package algoritmakodcalismalari;
+
+public class AlgoritmaKodCalismalari {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ Documents/NetBeansProjects/NTP/src/ATM/Main.java
@@ -0,0 +1,20 @@
+package ATM;
+
+
+public class Main {
+    public static void main(String[] args) {
+       
+        
+        ATM atm = new ATM();
+        
+        Hesap hesap = new Hesap("Furkan","12345", 2000);
+        
+        atm.calis(hesap);
+        System.out.println("Programdan Çıkılıyor...");
+        
+        
+        
+        
+    }
+ 
+}
