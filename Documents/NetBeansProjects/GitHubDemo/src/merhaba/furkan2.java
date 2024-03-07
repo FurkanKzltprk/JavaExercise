82185769aab3513e3001b344c339e44db31a5471
Furkan  kızıltoprak <kiziltoprakfurkan001@gmail.com>
2024-03-07 10:11:13 +0300

    selam

---
+++ Documents/NetBeansProjects/GitHubDemo/src/merhaba/furkan2.java
@@ -0,0 +1,14 @@
+package merhaba;
+
+public class furkan2 {
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        
+    }
+    
+}
