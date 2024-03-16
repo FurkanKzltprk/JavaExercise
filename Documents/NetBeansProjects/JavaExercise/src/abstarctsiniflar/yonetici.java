9deb6cfdc61f7bb988c03d4d5c208b3f110f81a7
Furkan  kızıltoprak <kiziltoprakfurkan001@gmail.com>
2024-03-16 12:20:16 +0300

    Abstract classlar ile ilgili basit düzey bir alıştırma.

---
+++ Documents/NetBeansProjects/JavaExercise/src/abstarctsiniflar/yonetici.java
@@ -0,0 +1,14 @@
+
+package abstarctsiniflar;
+
+
+public class yonetici extends Kullanici {
+
+    @Override
+    void kayitOl() {
+        System.out.println("Yonetici kayit edilidi.");
+    }
+
+    
+    
+}
